1ac8989416d3fcab4658cefe6f2bc8fccc3301ec
Green Applets <greenapplets191@gmail.com>
2015-04-14 10:26:14 +0800

    Sprint 3

--- Sprint 3 Results/Planimal/app/src/main/java/com/greenapplets/planimal/FragmentShop.java
+++ Sprint 3 Results/Planimal/app/src/main/java/com/greenapplets/planimal/FragmentShop.java
@@ -1,3 +1,42 @@
+/*
+*
+* Copyright (c) 2015, Green Applets
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+* this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+* this list of conditions and the following disclaimer in the documentation
+* and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+* OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Yohannah Bautista, Nicle Vynique Bedia, Algina Castillo
+*
+* This is a course requirement for CS 192 Software Engineering II
+* under the supervision of Asst. Prof. Ma. Rowena C. Solamo
+* of the Department of Computer Science, College of Engineering,
+* University of the Philippines, Diliman for the AY 2014-2015
+*
+*
+* Code History:
+*
+*
+*
+*/
+
 package com.greenapplets.planimal;
 
 import android.os.Bundle;
@@ -7,9 +46,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by ASUS on 3/14/2015.
- */
 public class FragmentShop extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
--- Sprint 3 Results/Planimal/app/src/main/java/com/greenapplets/planimal/MainActivity.java
+++ Sprint 3 Results/Planimal/app/src/main/java/com/greenapplets/planimal/MainActivity.java
@@ -1,3 +1,42 @@
+/*
+*
+* Copyright (c) 2015, Green Applets
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+* this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+* this list of conditions and the following disclaimer in the documentation
+* and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+* OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Yohannah Bautista, Nicle Vynique Bedia, Algina Castillo
+*
+* This is a course requirement for CS 192 Software Engineering II
+* under the supervision of Asst. Prof. Ma. Rowena C. Solamo
+* of the Department of Computer Science, College of Engineering,
+* University of the Philippines, Diliman for the AY 2014-2015
+*
+*
+* Code History:
+*
+*
+*
+*/
+
 package com.greenapplets.planimal;
 
 import android.app.Activity;
@@ -7,14 +46,6 @@ import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.FragmentActivity;
-/*
-* I'm really sorry that it took a really long time to make this
-* I wanted to remake everything because I realized I'd have a harder
-* time coding if I just stuck to the code as it is, so I took the
-* time to watch android tutorials from the start para di na magulo yung
-* UI ;w; Hopefully it should be similar to the original code so putting in
-* the DAO shouldn't be a problem? idk
-* */
 public class MainActivity extends FragmentActivity {
      ViewPager viewPager;
      public static DAO dao;
